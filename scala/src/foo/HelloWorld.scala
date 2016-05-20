446806c342d13118364dafdb0afe3bc428812541
Dave Roberts <dave.a.roberts@gmail.com>
2016-05-20 14:36:54 -0400

    Shortened up scala code

--- scala/src/foo/HelloWorld.scala
+++ scala/src/foo/HelloWorld.scala
@@ -2,62 +2,39 @@ package foo{
   object HelloWorld {
     def main(args: Array[String]): Unit = {
       println("Calling logic as a pure function")
-      var result = logic(
-        "param1",
-        "param2",
-        (level:String,msg:String)=>{},
-        (status:String)=>{},
-        (obj:Any)=>{},
-        ()=>{false},
-        (info:String)=>{});
+      var result = logic("param1", "param2")
       println("Result is: "+result)
       println("Calling logic as a regular function")
       // Regular invocation
       var result2 = logic(
         "param1",
         "param2",
-        (level:String,msg:String)=>{
-          // call your actual logging framework
-          println("LOG["+level+"] ["+msg+"]");
-        },
-        (status:String)=>{
-          // set a variable to check elsewhere
-          println("Current status set to: "+status)
-        },
-        (obj:Any)=>{
-          println("Called notify message on object: "+obj);
-        },
-        ()=>{
-          val dbLookupResult = false
-          dbLookupResult
-        },
-        (info:String)=>{
-          println("Info written to file [["+info+"]]");
-        });
+        (level:String,msg:String)=>{ println("LOG["+level+"] ["+msg+"]") },
+        (status:String)=>{ println("Current status set to: "+status) },
+        (obj:Any)=>{ println("Called notify message on object: "+obj) },
+        ()=>{ val dbLookupResult = false; dbLookupResult },
+        (info:String)=>{ println("Info written to file [["+info+"]]") });
       println("Result is: "+result2)
     }
 
     def logic(param1: String,
               param2: String,
-              log:(String, String)=>Unit,
-              setStatus:(String)=>Unit,
-              notify:(Any)=>Unit,
-              eStop:()=>Boolean,
-              saveFile:(String)=>Unit):String = {
-      setStatus("running")
+              log:(String, String)=>Unit = null,
+              setStatus:(String)=>Unit = null,
+              notify:(Any)=>Unit = null,
+              eStop:()=>Boolean = null,
+              saveFile:(String)=>Unit=null):String = {
+      if (setStatus != null){ setStatus("running") }
       val s1 = param1+"::"+param2
-      log("INFO", "About to do Step 2, this could take awhile")
+      if (log != null){ log("INFO", "About to do Step 2, this could take awhile") }
       val s2 = s1+"::step2"
-      log("INFO", "Completed step 2")
+      if (log != null){ log("INFO", "Completed step 2") }
       val s3 = s2+"::step3"
-      notify("randomobjecttonotify")
-      if (eStop()){
-        setStatus("stopped")
-        return "stoppedresult"
-      }
+      if (notify != null){ notify("randomobjecttonotify") }
+      if (eStop != null && eStop()){ setStatus("stopped"); return "stoppedresult" }
       val s4 = s3+"::step4"
-      setStatus("completed")
-      saveFile("Logic completed for params "+param1+"::"+param2)
+      if (setStatus != null){ setStatus("completed") }
+      if (saveFile != null){ saveFile("Logic completed for params "+param1+"::"+param2) }
       return s4
     }
   }
